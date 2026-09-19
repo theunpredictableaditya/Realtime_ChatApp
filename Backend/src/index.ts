4ecc796bd8917edd9358420414ae72bfc57e139d
Aditya Kumar Gupta <adityakgupta206@gmail.com>
2026-09-19 22:47:01 +0545

    Refactor server setup to use http.createServer and integrate socket.io for real-time communication

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,12 +1,26 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import http from "http"
+import { Server } from 'socket.io'
 import { app } from './app.js'
 import { connectDB } from './db/dbConnection.js'
 
+const server = http.createServer(app)
+
 const port = process.env.PORT || 3000
 
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+    console.log("User Connected: ", socket.id)
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected: ", socket.id)
+    })
+})
+
 connectDB().then(() => {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Server is running on port ${port}`)
     })
     .on('error', (error: unknown) => {
